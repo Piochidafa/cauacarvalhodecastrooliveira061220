6d081ea23b82a6e1e7eac423fe28f31ee753ae90
Cauã C4ju C C. Oliveira <cauacastrooliveira@outlook.com>
2026-01-20 23:51:24 -0300

    feat: adicionar controllers REST para Album, AlbumCover, Regional e Artista

--- pet-back/src/main/java/com/pet/api/controller/ArtistaController.java
+++ pet-back/src/main/java/com/pet/api/controller/ArtistaController.java
@@ -1,15 +1,13 @@
 package com.pet.api.controller;
 
+import com.pet.api.dto.ArtistaDTO;
 import com.pet.api.model.Artista;
 import com.pet.api.service.ArtistaService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/artista")
@@ -22,8 +20,29 @@ public class ArtistaController {
     @GetMapping
     public Page<Artista> retornaTodosArtistasPaginado(Pageable pageable){
 
-        return artistaService.retornaTodosArtistas(pageable);
+        return artistaService.getAllPaginado(pageable);
 
+    }
+
+    @PostMapping("/create")
+    public Artista createArtista(@RequestBody ArtistaDTO artista){
+
+        return artistaService.createArtista(artista);
+    }
+
+    @GetMapping("/{id}")
+    public Artista getArtistaById(@PathVariable Long id){
+        return artistaService.getById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Artista updateArtista(@PathVariable Long id, @RequestBody ArtistaDTO artista){
+        return artistaService.updateArtista(id, artista);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteArtista(@PathVariable Long id){
+        artistaService.deleteArtista(id);
     }
 
 }
