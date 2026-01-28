ac738629a4c2694a6ad710ebb797292409dee900
Cauã C4ju C C. Oliveira <cauacastrooliveira@outlook.com>
2026-01-28 01:42:19 -0300

    feat(db): adicionar colunas de imagem do artista

--- pet-back/src/main/resources/db/migration/V3__Create_table_artista.sql
+++ pet-back/src/main/resources/db/migration/V3__Create_table_artista.sql
@@ -1,6 +1,8 @@
 CREATE TABLE tb_artista (
     id          BIGSERIAL PRIMARY KEY,
     nome        VARCHAR(200) NOT NULL,
+    image_key   VARCHAR(500),
+    image_url   VARCHAR(1000),
     created_at  TIMESTAMP DEFAULT NOW(),
     updated_at  TIMESTAMP
 );
